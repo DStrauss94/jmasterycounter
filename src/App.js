fee537e476207bf7fd4b6fb55063700d233589ff
Douglas Strauss <douglasstrauss94@gmail.com>
2022-09-11 11:20:44 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+//basic counter app using useState and Useeffect
 
-function App() {
+import './App.css';
+import { useState, useEffect } from 'react';
+
+const App  =  () => {
+  const [counter , setCounter] = useState(0);
+
+   useEffect(() => {
+    alert(counter);
+   }, [counter]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App" >
+      <button onClick={() => setCounter((prevCount)=> prevCount - 1 )}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount)=> prevCount + 1 )}>+</button>
+
     </div>
   );
 }
-
 export default App;
